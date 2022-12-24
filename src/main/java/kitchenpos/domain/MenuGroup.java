33fb82e0417ca03a04e5e558f5a83bc410b24131
jhkang <serdozz@naver.com>
2022-12-25 02:05:32 +0900

    test(MenuGroup): 단위테스트 추가

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -1,22 +1,32 @@
 package kitchenpos.domain;
 
+import org.flywaydb.core.internal.util.StringUtils;
+
 public class MenuGroup {
     private Long id;
     private String name;
 
+    public MenuGroup(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    public MenuGroup(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
     public Long getId() {
         return id;
     }
-
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(final String name) {
-        this.name = name;
+    private void validateName(String name) {
+        if (!StringUtils.hasText(name)) {
+            throw new IllegalArgumentException("상품 이름은 필수값 입니다.");
+        }
     }
 }
