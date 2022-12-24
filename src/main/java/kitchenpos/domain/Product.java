a130f3301a196bd49d5c97e4abc627ced736199f
jhkang <serdozz@naver.com>
2022-12-25 00:27:54 +0900

    test(Product): 단위테스트 추가

--- src/main/java/kitchenpos/domain/Product.java
+++ src/main/java/kitchenpos/domain/Product.java
@@ -1,11 +1,22 @@
 package kitchenpos.domain;
 
 import java.math.BigDecimal;
+import java.util.Objects;
+import org.flywaydb.core.internal.util.StringUtils;
 
 public class Product {
     private Long id;
     private String name;
     private BigDecimal price;
+
+    public Product(){}
+
+    public Product(String name, BigDecimal price) {
+        validateName(name);
+        validatePrice(price);
+        this.name = name;
+        this.price = price;
+    }
 
     public Long getId() {
         return id;
@@ -30,4 +41,18 @@ public class Product {
     public void setPrice(final BigDecimal price) {
         this.price = price;
     }
+
+
+    private void validateName(String name) {
+        if (!StringUtils.hasText(name)) {
+            throw new IllegalArgumentException("상품 이름은 필수값 입니다.");
+        }
+    }
+
+    private void validatePrice(BigDecimal price) {
+        if (Objects.isNull(price) || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("상품가격은 필수값이며, 0보다 커야합니다.");
+        }
+    }
+
 }
