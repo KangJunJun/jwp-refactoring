649074839fd1eda24b78c2594a138a32aa0468f1
kangjunho <serdozz@naver.com>
2022-12-30 22:47:03 +0900

    refactor(Order): JPA적용

--- src/main/java/kitchenpos/order/domain/Order.java
+++ src/main/java/kitchenpos/order/domain/Order.java
@@ -1,36 +1,61 @@
 package kitchenpos.order.domain;
 
+import kitchenpos.ordertable.domain.OrderTable;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Entity
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long orderTableId;
-    private String orderStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private OrderTable orderTable;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @CreatedDate
     private LocalDateTime orderedTime;
     private List<OrderLineItem> orderLineItems;
+
+    protected Order(){
+    }
+
+    public Order(
+            OrderTable orderTable,
+            OrderStatus orderStatus
+    ) {
+        validate(orderTable);
+        this.orderTable = orderTable;
+        this.orderStatus = orderStatus;
+    }
+
+    private void validate(OrderTable orderTable) {
+        if (orderTable.isEmpty()) {
+            throw new IllegalArgumentException("OrderTable 이 비어있습니다.");
+        }
+    }
+
+
+    public OrderTable getOrderTable() {
+        return orderTable;
+    }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(final Long id) {
-        this.id = id;
-    }
-
-    public Long getOrderTableId() {
-        return orderTableId;
-    }
-
-    public void setOrderTableId(final Long orderTableId) {
-        this.orderTableId = orderTableId;
-    }
-
-    public String getOrderStatus() {
+    public OrderStatus getOrderStatus() {
         return orderStatus;
     }
 
-    public void setOrderStatus(final String orderStatus) {
+    public void setOrderStatus(final OrderStatus orderStatus) {
         this.orderStatus = orderStatus;
     }
 
@@ -38,15 +63,14 @@ public class Order {
         return orderedTime;
     }
 
-    public void setOrderedTime(final LocalDateTime orderedTime) {
-        this.orderedTime = orderedTime;
-    }
-
     public List<OrderLineItem> getOrderLineItems() {
         return orderLineItems;
     }
 
-    public void setOrderLineItems(final List<OrderLineItem> orderLineItems) {
+
+    public void setOrderLineItems(List<OrderLineItem> orderLineItems) {
         this.orderLineItems = orderLineItems;
+
+        orderLineItems.forEach(orderLineItem -> orderLineItem.setOrder(this));
     }
 }
